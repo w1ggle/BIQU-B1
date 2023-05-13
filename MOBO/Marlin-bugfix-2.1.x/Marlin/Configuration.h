af8a6637901bd53eccd64922d7db6c5ceeed8731
Isaiah <124836428+w1ggle@users.noreply.github.com>
2023-05-13 00:06:12 -0400

    PID to MPC

--- MOBO/Marlin-bugfix-2.1.x/Marlin/Configuration.h
+++ MOBO/Marlin-bugfix-2.1.x/Marlin/Configuration.h
@@ -675,8 +675,8 @@
  * PIDTEMP : PID temperature control (~4.1K)
  * MPCTEMP : Predictive Model temperature control. (~1.8K without auto-tune)
  */
-#define PIDTEMP           // See the PID Tuning Guide at https://reprap.org/wiki/PID_Tuning
-//#define MPCTEMP         // ** EXPERIMENTAL ** See https://marlinfw.org/docs/features/model_predictive_control.html
+//#define PIDTEMP    //IP default on       // See the PID Tuning Guide at https://reprap.org/wiki/PID_Tuning
+#define MPCTEMP     //IP default off       // ** EXPERIMENTAL ** See https://marlinfw.org/docs/features/model_predictive_control.html
 
 #define PID_MAX  255      // Limit hotend current while PID is active (see PID_FUNCTIONAL_RANGE below); 255=full current
 #define PID_K1     0.95   // Smoothing factor within any PID loop
@@ -716,7 +716,7 @@
   //#define MPC_AUTOTUNE_MENU                         // Add MPC auto-tuning to the "Advanced Settings" menu. (~350 bytes of flash)
 
   #define MPC_MAX 255                                 // (0..255) Current to nozzle while MPC is active.
-  #define MPC_HEATER_POWER { 40.0f }                  // (W) Heat cartridge powers.
+  #define MPC_HEATER_POWER { 40.0f } //IP double check this default                 // (W) Heat cartridge powers.
 
   #define MPC_INCLUDE_FAN                             // Model the fan speed?
 
